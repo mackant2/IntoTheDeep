26674fb4810810f42f4a2287b84fc5c4f6e9885f
evansgithubprojects <68164728+evansgithubprojects@users.noreply.github.com>
2025-01-15 16:02:02 -0800

    Update IntakeTester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/components/IntakeTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/components/IntakeTester.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.main.components;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
@@ -24,7 +26,40 @@ public class IntakeTester extends LinearOpMode {
 
         waitForStart();
 
+
+        int red1 = intakeColorSensor.red();
+        int green1 = intakeColorSensor.green();
+        int blue1 = intakeColorSensor.blue();
+
+
+        float[] hsvValues1 = new float[3];
+        Color.RGBToHSV(red1, green1, blue1, hsvValues1);
+        float hue1 = hsvValues1[0];
+        float saturation1 = hsvValues1[1];
+        float value1 = hsvValues1[2];
         robot = new Robot(this, parsedHardwareMap, true);
+
+
+        float minSaturation = 0.4f; // Adjust as necessary
+        float minValue = 0.4f; // Adjust as necessary
+
+        if (saturation1 >= minSaturation && value1 >= minValue) {
+            if (hue1 >= 0 && hue1 < 65) {
+                telemetry.addData("Color 1", "Red");
+                robot.intake.SetIntakeState((Intake.IntakeState.DriverControlled));
+            } else if (hue1 >= 65 && hue1 < 100) {
+                telemetry.addData("Color 1", "Yellow");
+                robot.intake.SetIntakeState((Intake.IntakeState.DriverControlled));
+            } else if (hue1 >= 165 && hue1 < 240) {
+                telemetry.addData("Color 1", "Blue");
+                robot.intake.SetIntakeState((Intake.IntakeState.DriverControlled));
+            } else {
+                telemetry.addData("Color 1", "Unknown");
+            }
+        } else {
+            telemetry.addData("Color 1", "Unknown");
+        }
+
 
         if(gamepad1.dpad_left){
             robot.intake.SetIntakeState((Intake.IntakeState.Intaking));
